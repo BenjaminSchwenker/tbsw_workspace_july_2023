7f38148f8c3ea7a2db1e288f82622bfe951f43cf
bgnet <you@example.com>
2023-07-09 15:14:04 +0200

    update to auto scan/plot script

--- scan_runs.py
+++ scan_runs.py
@@ -1,20 +1,47 @@
 import pandas as pd
 import os
 import argparse
+import subprocess
+from multiprocessing import Pool
 
 
-def main(start, stop):
+def run_script(input):
+    (command, arg_str) = input
+    print("Starting command :{} with argument {}".format(command, arg_str))
+    result = subprocess.call(command+" -e "+arg_str, shell=True)
+    print("Completed command :{} with argument {}".format(command, arg_str))
+    return result
+
+
+def main(start, stop, only_plot, only_scan):
 
     df = pd.read_csv("runDB.csv", sep=" ")
     df = df[(df["runno"] >= start) & (df["runno"] <= stop)]
-    for index in df.index:
-        os.system(
-            f"python3 tj2-reco.py --runno {df.iloc[index]['runno']}  --gearfile gear_geoid{df.iloc[index]['geoID']}.xml"
-        )
-        os.system(
-            f"python3 histo-plotter-tj2.py --runno {df.iloc[index]['runno']}  --colstart {df.iloc[index]['roi_colstart']} --colstop {df.iloc[index]['roi_colstop']} --rowstart {df.iloc[index]['roi_rowstart']} --rowstop {df.iloc[index]['roi_rowstop']}"
-        )
+    args_scan = []
+    args_plot = []
 
+    if not only_scan:
+        for index in df.index:
+            args_plot.append(
+                f"python3 histo-plotter-tj2.py --runno {int(df.iloc[index]['runno'])}  --colstart {int(df.iloc[index]['roi_colstart'])} --colstop {int(df.iloc[index]['roi_colstop'])} --rowstart {int(df.iloc[index]['roi_rowstart'])} --rowstop {int(df.iloc[index]['roi_rowstop'])}")
+
+    df = df.drop_duplicates(subset=['runno'])
+    if not only_plot:
+        for index in range(len(df.index)):
+            print(index)
+            args_scan.append(
+                f"python3 tj2-reco.py --runno {int(df.iloc[index]['runno'])}  --gearfile gear_geoid{int(df.iloc[index]['geoID'])}.xml --minocc {df.iloc[index]['minocc']}")
+    
+
+    with Pool(4) as p:  # choose appropriate level of parallelism
+        # choose appropriate command and argument, can be fetched from sys.argv if needed
+        exit_codes = p.map(run_script, [('xterm', arg) for arg in args_scan])
+        print("Exit codes : {}".format(exit_codes))
+
+    with Pool(4) as p:  # choose appropriate level of parallelism
+        # choose appropriate command and argument, can be fetched from sys.argv if needed
+        exit_codes = p.map(run_script, [('xterm', arg) for arg in args_plot])
+        print("Exit codes : {}".format(exit_codes))
 
 if __name__ == '__main__':
 
@@ -24,5 +51,9 @@ if __name__ == '__main__':
                         type=int, help='Start run number')
     parser.add_argument('--stop', dest='stop', type=int,
                         help='Stop run number')
+    parser.add_argument('--only_plot', dest='only_plot', type=bool, default=False,
+                        help='only do plotting')
+    parser.add_argument('--only_scan', dest='only_scan', type=bool, default=False,
+                        help='only do reco')
     args = parser.parse_args()
-    main(args.start, args.stop)
+    main(args.start, args.stop, args.only_plot, args.only_scan)
