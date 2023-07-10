39cab50107c3037cf4221b67fca2c4d23ccfb906
bgnet <you@example.com>
2023-07-10 18:00:38 +0200

    added full rundb and added clusterdb as standard to script

--- scan_runs.py
+++ scan_runs.py
@@ -15,13 +15,13 @@ def run_script(input):
 
 def main(start, stop, only_plot, only_scan):
 
-    df = pd.read_csv("runDB.csv", sep=" ")
+    df = pd.read_csv("runDB.csv", sep=",")
     df = df[(df["runno"] >= start) & (df["runno"] <= stop)]
     args_scan = []
     args_plot = []
-
+    print(df)
     if not only_scan:
-        for index in df.index:
+        for index in range(len(df.index)):
             args_plot.append(
                 f"python3 histo-plotter-tj2.py --runno {int(df.iloc[index]['runno'])}  --colstart {int(df.iloc[index]['roi_colstart'])} --colstop {int(df.iloc[index]['roi_colstop'])} --rowstart {int(df.iloc[index]['roi_rowstart'])} --rowstop {int(df.iloc[index]['roi_rowstop'])}")
 
@@ -30,7 +30,7 @@ def main(start, stop, only_plot, only_scan):
         for index in range(len(df.index)):
             print(index)
             args_scan.append(
-                f"python3 tj2-reco.py --runno {int(df.iloc[index]['runno'])}  --gearfile gear_geoid{int(df.iloc[index]['geoID'])}.xml --minocc {df.iloc[index]['minocc']}")
+                f"python3 tj2-reco.py --runno {int(df.iloc[index]['runno'])}  --gearfile gear_geoid{int(df.iloc[index]['geoID'])}.xml --minocc {df.iloc[index]['minocc']} --prefix _clustdb")
     
 
     with Pool(4) as p:  # choose appropriate level of parallelism
